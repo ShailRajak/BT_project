c127d5f2ba2731e0639ef6ea53d5e1ee5a6c2d8a
ShailRajak <shailendrarajak788@gmail.com>
2025-12-15 11:21:53 +0530

    Fix 500 error by removing invalid created_at ordering

--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,19 +6,20 @@ from .models import User
 from .serializers import UserSerializer
 from django.shortcuts import get_object_or_404
 
-@api_view(["GET"])
-def list_users(request):
-    users = User.objects.all().order_by("created_at")
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+@api_view(["GET", "POST"])
+def users_list_create(request):
 
-@api_view(["POST"])
-def create_user(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["PATCH"])
 def update_user_partial(request, pk):
